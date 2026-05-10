2602da7c72891d1152e9b92c09b1958147028fae
Mahjoub07 <mahjoubbounhar@gmail.com>
2026-05-10 21:21:03 +0100

    fix: authcontroller

--- backend/src/main/java/com/interventions/backend/controller/AuthController.java
+++ backend/src/main/java/com/interventions/backend/controller/AuthController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://mahjoub07.github.io"})
 @RequiredArgsConstructor
 public class AuthController {
 
